11a5ed154434a039a9377cda8cfc9f1b93d730a7
deveon95 <51299713+deveon95@users.noreply.github.com>
2024-07-07 17:13:41 +0100

    Add button code and test

--- insertomatic-pico.cpp
+++ insertomatic-pico.cpp
@@ -3,6 +3,14 @@
 #include "lcd_display.hpp"
 
 #include "hardware/gpio.h"
+#include "pico/time.h"
+
+#define NO_OF_CHANNELS 6
+
+#define BUTTON_F 6
+#define BUTTON_U 7
+#define BUTTON_D 8
+#define BUTTON_INTERVAL 50
 
 #define I2C_SCL_PIN 16 // Define SCL pin
 #define I2C_SDA_PIN_1 10 // Define SDA pin
@@ -14,6 +22,11 @@
 int I2C_SDA_PINS[] = {I2C_SDA_PIN_1, I2C_SDA_PIN_2, I2C_SDA_PIN_3, I2C_SDA_PIN_4, I2C_SDA_PIN_5, I2C_SDA_PIN_6};
 #define I2C_ADDRESS (0xCA >> 1) // Define I2C address
 #define I2C_DELAY 5 // Delay in microseconds for timing
+
+#define FN_IDLE 0
+#define FN_CHANNELS 1
+#define FN_STANDARD 2
+#define FN_TESTPATTERN 3
 
 void gpio_put_all_sda_pins(bool value)
 {
@@ -60,7 +73,7 @@ void i2c_stop() {
 }
 
 void i2c_write_bits(bool bits[]) {
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < NO_OF_CHANNELS; i++) {
         if (bits[i] == 0)
         {
             gpio_set_dir(I2C_SDA_PINS[i], true);
@@ -89,9 +102,9 @@ bool i2c_read_ack() {
 }
 
 bool i2c_write_bytes(uint8_t bytes[][4], uint8_t byteno) {
-    bool bits[6];
+    bool bits[NO_OF_CHANNELS];
     for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 6; j++) {
+        for (int j = 0; j < NO_OF_CHANNELS; j++) {
             bits[j] = (bytes[j][byteno] >> (7 - i)) & 0x01;
         }
         i2c_write_bits(bits);
@@ -100,9 +113,9 @@ bool i2c_write_bytes(uint8_t bytes[][4], uint8_t byteno) {
 }
 
 bool i2c_write_byte(uint8_t byte) {
-    bool bits[6];
+    bool bits[NO_OF_CHANNELS];
     for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 6; j++) {
+        for (int j = 0; j < NO_OF_CHANNELS; j++) {
             bits[j] = byte & 0x80;
         }
         byte <<= 1;
@@ -132,65 +145,105 @@ LCDdisplay myLCD(5,4,3,2,1,0,24,2); // DB4, DB5, DB6, DB7, RS, E, character_widt
 
 int main() {
 	myLCD.init();
-	while (true) {
-		myLCD.clear();
-		myLCD.print("Insertomatic 6000 ------");
-		sleep_ms(2500);
-		myLCD.goto_pos(0,0);
-		myLCD.print(" P1  P2  P3  P4  P5  P6 ");
-		myLCD.goto_pos(0,1);
-		myLCD.print(" C21 C24 C27 C31 C34 C37");
-		sleep_ms(2500);
-		myLCD.goto_pos(0,0);
-		myLCD.print("P1 [Station name]       ");
-		myLCD.goto_pos(0,1);
-		myLCD.print("[Now playing info]      ");
-		//sleep_ms(2500);
+    myLCD.clear();
+    myLCD.print("Insertomatic 6000 ------");
+    sleep_ms(2500);
+    myLCD.goto_pos(0,0);
+    myLCD.print(" P1  P2  P3  P4  P5  P6 ");
+    myLCD.goto_pos(0,1);
+    myLCD.print(" C21 C23 C25 C27 C29 C31");
+    sleep_ms(2500);
+    //myLCD.goto_pos(0,0);
+    //myLCD.print("P1 [Station name]       ");
+    //myLCD.goto_pos(0,1);
+    //myLCD.print("[Now playing info]      ");
+    //sleep_ms(2500);
 
-		gpio_init(I2C_SCL_PIN);
-		gpio_set_dir(I2C_SCL_PIN, GPIO_IN);
-		gpio_put(I2C_SCL_PIN, 0);
+    gpio_init_mask((1 << BUTTON_F) | (1 << BUTTON_D) | (1 << BUTTON_U));
+    gpio_set_pulls(BUTTON_F, true, false);
+    gpio_set_pulls(BUTTON_U, true, false);
+    gpio_set_pulls(BUTTON_D, true, false);
 
-		gpio_init(I2C_SDA_PIN_1);
-		gpio_init(I2C_SDA_PIN_2);
-		gpio_init(I2C_SDA_PIN_3);
-		gpio_init(I2C_SDA_PIN_4);
-		gpio_init(I2C_SDA_PIN_5);
-		gpio_init(I2C_SDA_PIN_6);
-		gpio_set_dir(I2C_SDA_PIN_1, GPIO_IN);
-		gpio_set_dir(I2C_SDA_PIN_2, GPIO_IN);
-		gpio_set_dir(I2C_SDA_PIN_3, GPIO_IN);
-		gpio_set_dir(I2C_SDA_PIN_4, GPIO_IN);
-		gpio_set_dir(I2C_SDA_PIN_5, GPIO_IN);
-		gpio_set_dir(I2C_SDA_PIN_6, GPIO_IN);
-		gpio_put_all_sda_pins(0);
+    gpio_init(I2C_SCL_PIN);
+    gpio_set_dir(I2C_SCL_PIN, GPIO_IN);
+    gpio_put(I2C_SCL_PIN, 0);
 
-		uint8_t data[6][4];
-        /* = {
-            {0x80, 0x10, 0x5D, 0x74}, // 21
-            {0x80, 0x10, 0x5E, 0x74}, // 23
-            {0x80, 0x10, 0x5F, 0x74}, // 25
-            {0x80, 0x10, 0x60, 0x74}, // 27
-            {0x80, 0x10, 0x61, 0x74}, // 29
-            {0x80, 0x10, 0x62, 0x74}  // 31
-        };*/
+    gpio_init(I2C_SDA_PIN_1);
+    gpio_init(I2C_SDA_PIN_2);
+    gpio_init(I2C_SDA_PIN_3);
+    gpio_init(I2C_SDA_PIN_4);
+    gpio_init(I2C_SDA_PIN_5);
+    gpio_init(I2C_SDA_PIN_6);
+    gpio_set_dir(I2C_SDA_PIN_1, GPIO_IN);
+    gpio_set_dir(I2C_SDA_PIN_2, GPIO_IN);
+    gpio_set_dir(I2C_SDA_PIN_3, GPIO_IN);
+    gpio_set_dir(I2C_SDA_PIN_4, GPIO_IN);
+    gpio_set_dir(I2C_SDA_PIN_5, GPIO_IN);
+    gpio_set_dir(I2C_SDA_PIN_6, GPIO_IN);
+    gpio_put_all_sda_pins(0);
 
-        uint8_t standard = 2;
-        bool testpattern = true;
-        uint8_t channels[] = {21, 23, 25, 27, 29, 31};
-        for (uint8_t i = 0; i < 6; i++) {
-            data[i][0] = 0x80;
-            data[i][1] = standard << 3;
-            uint16_t desired_frequency = (channels[i] - 21) * 4 * 8 + 1885;
-            uint16_t desired_n = (desired_frequency << 2) & 0x3ffc;
-            data[i][2] = (desired_n >> 8) | (testpattern << 6);
-            data[i][3] = desired_n & 0xff;
+    // Modulator data array
+    uint8_t data[NO_OF_CHANNELS][4];
+    /* Example array data
+        = {
+        {0x80, 0x10, 0x5D, 0x74}, // 21
+        {0x80, 0x10, 0x5E, 0x74}, // 23
+        {0x80, 0x10, 0x5F, 0x74}, // 25
+        {0x80, 0x10, 0x60, 0x74}, // 27
+        {0x80, 0x10, 0x61, 0x74}, // 29
+        {0x80, 0x10, 0x62, 0x74}  // 31
+    };*/
+
+    uint8_t standard = 2;
+    bool testpattern = true;
+    uint8_t channels[] = {21, 23, 25, 27, 29, 31};
+    for (uint8_t i = 0; i < NO_OF_CHANNELS; i++) {
+        data[i][0] = 0x80;
+        data[i][1] = standard << 3;
+        uint16_t desired_frequency = (channels[i] - 21) * 4 * 8 + 1885;
+        uint16_t desired_n = (desired_frequency << 2) & 0x3ffc;
+        data[i][2] = (desired_n >> 8) | (testpattern << 6);
+        data[i][3] = desired_n & 0xff;
+    }
+
+    i2c_bitbang_write(I2C_ADDRESS, data, 4);
+
+    uint32_t lastms = 0;
+    uint32_t function = FN_IDLE;
+    bool buttonFLast = 1;
+    bool buttonULast = 1;
+    bool buttonDLast = 1;
+
+    // Main loop
+    while (true)
+    {
+        if (to_ms_since_boot(get_absolute_time()) > lastms + BUTTON_INTERVAL)
+        {
+            lastms = to_ms_since_boot(get_absolute_time());
+
+            bool buttonFState = gpio_get(BUTTON_F);
+            bool buttonUState = gpio_get(BUTTON_U);
+            bool buttonDState = gpio_get(BUTTON_D);
+
+            if (!buttonFState && buttonFLast)
+            {
+                switch (function)
+                {
+                    case FN_IDLE:
+                    function = FN_CHANNELS;
+                    break;
+
+                    case FN_CHANNELS:
+                    function = FN_IDLE;
+                    break;
+                }
+                myLCD.goto_pos(0,0);
+                myLCD.write('0' + function);
+            }
+            buttonFLast = buttonFState;
+            buttonULast = buttonUState;
+            buttonDLast = buttonDState;
         }
-
-		while (true) {
-			i2c_bitbang_write(I2C_ADDRESS, data, 4);
-			sleep_ms(1000); // Write every second
-		}
-	};
+    }
 	return 0;
 };
